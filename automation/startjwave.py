914d4323e30aab1642544b9cc8950ae6bdf0b8b3
makoto <mmmagic@gmail.com>
2023-09-19 18:33:00 +0900

    updated for newer version of selenium

--- automation/startjwave.py
+++ automation/startjwave.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
-import chromedriver_binary
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome('chromedriver', options=options)
+driver = webdriver.Chrome(options=options)
 driver.get('http://radiko.jp/#!/live/FMJ')
-time.sleep(10)
-element = driver.find_element_by_class_name('js-policy-accept')
+time.sleep(5)
+element = driver.find_element(By.CLASS_NAME, 'js-policy-accept')
 element.click()
-element = driver.find_element_by_class_name('play-radio')
+time.sleep(1)
+element = driver.find_element(By.CLASS_NAME, 'play-radio')
 element.click()
 time.sleep(3600)
 driver.quit()
